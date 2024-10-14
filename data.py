3ca136c24baf1de7502a33f8d649205ee20a1344
IaOksana <iashchuk.oksana@gmail.com>
2024-10-14 15:36:04 +0400

    Removed unused files, fixed some minor bugs

--- data.py
+++ data.py
@@ -1,8 +1,21 @@
 import os, re
+from pathlib import Path
+
 
 def load_data(filename: str) -> list[str]:
-    with open(filename, "r") as file:
-        return file.readlines()
+    
+    path = Path(filename)
+
+    if path.exists() :
+        with open(filename, "r") as file:
+            return file.readlines()
+    else :
+        print("No such file")
+        return []
 
 def clean_data(employees_data: list[str]) -> list[float]:
-    return [int(re.sub(r"[^\d]", '', temp.strip())) for temp in employees_data if temp.strip()]
+    
+    if employees_data :
+        return [float(re.sub(r"[^\d]", '', temp.strip())) for temp in employees_data if temp.strip()]
+    else :
+        return []
--- processing.py
+++ processing.py
@@ -1,12 +1,12 @@
 # Обрахуйте загальну суму заробітної плати, а потім розділіть її на кількість 
 # розробників, щоб отримати середню зарплату.
 
-def calculate_statistics (employees : list[float] = None) -> tuple:
+def calculate_statistics (employees : list[float] = None) -> tuple[float, float]:
     
     if not employees:
-        return None
+        return (0, 0)
  
-    return {
+    return (
         sum(employees),
         sum(employees) / len(employees),
-    }
+    )
